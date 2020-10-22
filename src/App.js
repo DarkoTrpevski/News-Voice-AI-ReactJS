fb7068f47e58e60317d724b77cb11168b9686b5f
Darko <darko_trpevski@yahoo.com>
2020-10-22 10:24:01 +0200

    Creating the news cards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+import NewsCards from './components/NewsCards/NewsCards'
+import useStyles from './styles';
 
-function App() {
+
+const alanKey = 'b4cca9e5a4e9b792f6e33aec98260be32e956eca572e1d8b807a3e2338fdd0dc/stage';
+
+const App = () => {
+
+  const classes = useStyles();
+
+  const[newsArticles, setnewsArticles] = useState([]);
+
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command, articles }) => {
+        if(command === 'newHeadLines') {
+          setnewsArticles(articles);
+        }
+      }
+    })
+  }, [])
+
   return (
     <div className="App">
+      <div className = {classes.logoContainer}>
+        <img className = {classes.alanLogo} src = "https://alan.app/voice/images/previews/preview.jpg" alt = "Alan AI Logo" />
+      </div>
+      <NewsCards articles = {newsArticles} />
     </div>
   );
 }
